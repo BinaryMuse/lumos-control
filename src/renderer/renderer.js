da549bc4e91abeb68bf741662f5092ec07cec40e
Michelle Tilley <michelle@michelletilley.net>
2018-10-27 22:02:52 -0700

    Rewrite App as function

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Spring, Transition } from 'react-spring'
 
@@ -6,74 +6,64 @@ import FullScreen from './ui/full-screen'
 import DeviceSelect from './device-select'
 import DeviceManager from './device-manager'
 
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      loadTimer: false,
-      selectedDevice: null
-    }
+function LoadingScreen ({ styles }) {
+  return (
+    <FullScreen style={{ ...styles, display: 'flex' }} key='loading'>
+      <Spring config={{ delay: 500, tension: 100, friction: 50 }} from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {innerStyles => (
+          <div style={{ ...innerStyles, flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ textAlign: 'center' }}>L U M O S</h1>
+          </div>
+        )}
+      </Spring>
+    </FullScreen>
+  )
+}
+
+function DeviceManagerScreen ({ device, styles }) {
+  return (
+    <FullScreen style={{ ...styles, display: 'flex' }} key='dev-manager'>
+      <DeviceManager device={device} />
+    </FullScreen>
+  )
+}
+
+function DeviceSelectScreen ({ handleSelectDevice, styles }) {
+  return (
+    <FullScreen style={{ ...styles, display: 'flex' }} key='dev-select'>
+      <DeviceSelect onSelectDevice={handleSelectDevice} />
+    </FullScreen>
+  )
+}
+
+function App () {
+  const [loaded, setLoaded] = useState(false)
+  const [selectedDevice, setSelectedDevice] = useState(null)
+
+  useEffect(() => {
+    // 3 second splash screen
+    setTimeout(() => setLoaded(true), 3000)
+  }, [])
+
+  let screen = null
+  if (!loaded) {
+    screen = <LoadingScreen />
+  } else if (selectedDevice) {
+    screen = <DeviceManagerScreen device={selectedDevice} />
+  } else {
+    screen = <DeviceSelectScreen handleSelectDevice={setSelectedDevice} />
   }
 
-  async componentDidMount () {
-    setTimeout(() => {
-      this.setState({ loadTimer: true })
-    }, 3000)
-  }
-
-  render () {
-    const loaded = this.state.loadTimer
-
-    return (
-      <Transition from={{ opacity: 0 }} enter={{ opacity: 1 }} leave={{ opacity: 0 }}>
-        {(loaded ? styles => this.renderApp(styles) : styles => this.renderLoading(styles))}
-      </Transition>
-    )
-  }
-
-  renderLoading (styles) {
-    return (
-      <FullScreen style={{ ...styles, display: 'flex' }} key='loading'>
-        <Spring config={{ delay: 500, tension: 100, friction: 50 }} from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          {innerStyles => (
-            <div style={{ ...innerStyles, flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-              <h1 style={{ textAlign: 'center' }}>L U M O S</h1>
-            </div>
-          )}
-        </Spring>
-      </FullScreen>
-    )
-  }
-
-  renderApp (styles) {
-    const dev = this.state.selectedDevice
-    return (
-      <Transition from={{ opacity: 0 }} enter={{ opacity: 1 }} leave={{ opacity: 0 }}>
-        {(dev ? styles => this.renderDeviceManager(styles) : styles => this.renderDeviceSelect(styles))}
-      </Transition>
-    )
-  }
-
-  renderDeviceSelect (styles) {
-    return (
-      <FullScreen style={{ ...styles, display: 'flex' }} key='dev-select'>
-        <DeviceSelect onSelectDevice={this.handleSelectDevice.bind(this)} />
-      </FullScreen>
-    )
-  }
-
-  renderDeviceManager (styles) {
-    return (
-      <FullScreen style={{ ...styles, display: 'flex' }} key='dev-manager'>
-        <DeviceManager device={this.state.selectedDevice} />
-      </FullScreen>
-    )
-  }
-
-  handleSelectDevice (device) {
-    console.log('Selected device', device)
-    this.setState({ selectedDevice: device })
-  }
+  return (
+    <Transition
+      items={[screen]}
+      keys={item => item.type}
+      from={{ opacity: 0 }}
+      enter={{ opacity: 1 }}
+      leave={{ opacity: 0 }}>
+      {item => styles => React.cloneElement(item, { styles })}
+    </Transition>
+  )
 }
 
 const render = () => ReactDOM.render(<App />, document.getElementById('app'))

e0beb2825d29875b6d36e93204781bced874cb42
Michelle Tilley <michelle@michelletilley.net>
2018-10-27 23:16:54 -0700

    Fade through white to give slight pause when transitioning screens

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Spring, Transition } from 'react-spring'
+import { Transition } from 'react-spring'
 
 import FullScreen from './ui/full-screen'
 import DeviceSelect from './device-select'
 import DeviceManager from './device-manager'
 
+function useTimeout (fn, delay, deps = []) {
+  useEffect(() => {
+    const timeout = setTimeout(fn, delay)
+    return () => clearTimeout(timeout)
+  }, deps)
+}
+
 function LoadingScreen ({ styles }) {
   return (
-    <FullScreen style={{ ...styles, display: 'flex' }} key='loading'>
-      <Spring config={{ delay: 500, tension: 100, friction: 50 }} from={{ opacity: 0 }} to={{ opacity: 1 }}>
-        {innerStyles => (
-          <div style={{ ...innerStyles, flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <h1 style={{ textAlign: 'center' }}>L U M O S</h1>
-          </div>
-        )}
-      </Spring>
+    <FullScreen style={{ ...styles, justifyContent: 'center', alignItems: 'center' }}>
+      <h1 style={{ textAlign: 'center' }}>L U M O S</h1>
     </FullScreen>
   )
 }
 
 function DeviceManagerScreen ({ device, styles }) {
   return (
-    <FullScreen style={{ ...styles, display: 'flex' }} key='dev-manager'>
+    <FullScreen style={styles}>
       <DeviceManager device={device} />
     </FullScreen>
   )
@@ -30,8 +31,34 @@ function DeviceManagerScreen ({ device, styles }) {
 
 function DeviceSelectScreen ({ handleSelectDevice, styles }) {
   return (
-    <FullScreen style={{ ...styles, display: 'flex' }} key='dev-select'>
+    <FullScreen style={styles}>
       <DeviceSelect onSelectDevice={handleSelectDevice} />
+    </FullScreen>
+  )
+}
+
+function FadeThroughWhite ({ styles, children }) {
+  const [timedOut, setTimedOut] = useState(false)
+
+  useTimeout(() => setTimedOut(true), 500)
+
+  let element = null
+  if (timedOut) {
+    element = React.Children.only(children)
+  }
+
+  return (
+    <FullScreen style={{ ...styles, display: 'flex' }}>
+      <Transition
+        items={element}
+        keys={item => item && item.key}
+        from={{ opacity: 0 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}>
+        {item => styles => {
+          return item && React.cloneElement(item, { styles })
+        }}
+      </Transition>
     </FullScreen>
   )
 }
@@ -40,28 +67,25 @@ function App () {
   const [loaded, setLoaded] = useState(false)
   const [selectedDevice, setSelectedDevice] = useState(null)
 
-  useEffect(() => {
-    // 3 second splash screen
-    setTimeout(() => setLoaded(true), 3000)
-  }, [])
+  useTimeout(() => setLoaded(true), 3000)
 
   let screen = null
   if (!loaded) {
-    screen = <LoadingScreen />
+    screen = <LoadingScreen key='loading' />
   } else if (selectedDevice) {
-    screen = <DeviceManagerScreen device={selectedDevice} />
+    screen = <DeviceManagerScreen device={selectedDevice} key='device-manager' />
   } else {
-    screen = <DeviceSelectScreen handleSelectDevice={setSelectedDevice} />
+    screen = <DeviceSelectScreen handleSelectDevice={setSelectedDevice} key='device-select' />
   }
 
   return (
     <Transition
       items={[screen]}
-      keys={item => item.type}
-      from={{ opacity: 0 }}
+      keys={item => item.key}
+      from={{ opacity: 1 }}
       enter={{ opacity: 1 }}
       leave={{ opacity: 0 }}>
-      {item => styles => React.cloneElement(item, { styles })}
+      {item => styles => <FadeThroughWhite key={item.key} styles={styles}>{item}</FadeThroughWhite>}
     </Transition>
   )
 }
